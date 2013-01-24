f07bf5832c13926cdc7395d44dbd345a38fae0c9
Terry Miyajima <tmiyajima@honolulu.gov>
2013-01-23 14:17:04 -1000

    Test config options

--- config/application.rb
+++ config/application.rb
@@ -56,10 +56,10 @@ module Honoluluanswers
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2.2'
+    config.assets.version = '1.2.3'
 
     # get precompilation working on heroku:
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     config.serve_static_assets = true
 
